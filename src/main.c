201e76951b7abc6b229b7a8457ffdbb294ad054b
eldemirberkay0 <99734746+eldemirberkay0@users.noreply.github.com>
2025-11-08 01:03:33 +0300

    .exe added to .gitignore

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <raylib.h>
 
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
 int main()
 {
-    printf("test");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TEST");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("DEMO SCREEN", 250, 250, 60, GREEN);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
