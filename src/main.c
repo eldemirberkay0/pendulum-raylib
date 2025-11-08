7dff8b7607ed55c37de6fbfab1f972e4f66653fb
eldemirberkay0 <99734746+eldemirberkay0@users.noreply.github.com>
2025-11-08 23:12:14 +0300

    CMakeLists updated, circle template added.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <raylib.h>
+#include "circle.h"
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#define SCREEN_WIDTH 1120
+#define SCREEN_HEIGHT 780
 
 int main()
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TEST");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "RAYLIB KANZII");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("DEMO SCREEN", 250, 250, 60, GREEN);
+        ClearBackground(RAYWHITE);
+
+        DrawLine(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2 + 50, BLACK);
+
+        DrawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 10, BLACK);
+
+        DrawCircle(SCREEN_WIDTH / 2 + 50, SCREEN_HEIGHT / 2 + 50, 20, RED);
+
         EndDrawing();
     }
 
